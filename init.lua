3de03a3827fce7fbfe471ca4f6ffb538a98899dc
nikola <you@example.com>
2026-03-25 23:18:23 -0400

    splitting config file

--- init.lua
+++ init.lua
@@ -19,33 +19,9 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local opts = {}
-local plugins = {
-{ "alexmozaidze/palenight.nvim", name = "palenight" },
-{
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' }
-},
-{
-    'nvim-telescope/telescope.nvim', version = '*',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        -- optional but recommended
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    }
-},
-{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-{"nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons", -- optional, but recommended
-    },
-    lazy = false, -- neo-tree will lazily load itself
-  }
-}
 
-require("lazy").setup(plugins, opts)
+require("lazy").setup("plugins")
+
 require("lualine").setup()
 require("nvim-treesitter").setup()
 require("nvim-treesitter").install {"lua","javascript", "c", "c_sharp", "css", "html", "python"}
@@ -57,6 +33,6 @@ vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' 
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
 vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left toggle <CR>')
-vim.keymap.set('n', '<leader>f', ':Neotree focus <CR>')
+vim.keymap.set('n', '<leader>f', ':Neotree filesystem focus  <CR>')
 
 vim.cmd.colorscheme "palenight"
