d1c0be00db8a5073e2ec3da695a5c49fdff462e9
Nikola <nikola.kisiov@local.com>
2026-09-16 22:37:59 -0400

    fix: removed redundant keymaps

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,9 +1,5 @@
 require("mason").setup()
 
-vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = "Go to definition" })
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "Format Local buffer" })
-vim.keymap.set("n", "<leader>df", vim.diagnostic.open_float, { desc = "Show line diagnostics" })
-
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = vim.tbl_deep_extend("force", capabilities, require("mini.completion").get_lsp_capabilities())
 
